80a98dc00985a83abafa2d8717c2f4f2c619f90f
Jakub Vasicek <j.vasicek58@gmail.com>
2023-09-14 14:28:10 +0200

    fix protein coverage stats

--- src/common.py
+++ src/common.py
@@ -199,7 +199,7 @@ def get_protein_coverage(data):
     current_type = -1
 
     active_regions = []
-    eventQ = []
+    eventQ = [] if (data[0][0] == 0) else [{'pos': 0, 'event': 'start', 'type': -1, 'pep_idx': -1}, {'pos': data[0][0], 'event': 'end', 'type': -1, 'pep_idx': -1}]
 
     for idx, pep in enumerate(data):
         eventQ.append({ 'pos': pep[0], 'event': 'start', 'type': pep[2], 'pep_idx': idx })
